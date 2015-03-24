a70d64019d76f7ebe1102b0cb4f9a5feddca53ae
Connor Young <cdy3xv@virginia.edu>
2015-03-24 19:11:24 -0400

    Added login page and base template

--- SecureWitness/SecureWitness/urls.py
+++ SecureWitness/SecureWitness/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.contrib import admin, auth
 
 urlpatterns = patterns('',
     url(r'^$', 'Report.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^accounts/changepw/$', 'django.contrib.auth.views.password_change', name='password_change'),
 )
