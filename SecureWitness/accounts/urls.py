06c5cef5b31fab18378569b6292a025239862d64
Connor Young <cdy3xv@virginia.edu>
2015-03-26 17:06:27 -0400

    Register WIP 1

--- SecureWitness/accounts/urls.py
+++ SecureWitness/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
 
+from accounts import views
+
 
 urlpatterns = patterns('',
 	url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^changepw/$', 'django.contrib.auth.views.password_change', name='password_change'),
+    url(r'^register/$', views.register, name='register'),
 )
--- SecureWitness/accounts/views.py
+++ SecureWitness/accounts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
+
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+
 # Create your views here.
+
+@sensitive_post_parameters()
+@csrf_protect
+@never_cache
+def register(request, authentication_form=AuthenticationForm):
+
+    if request.method == "POST":
+    	# Add User Model instance here
+    	form = authentication_form(request, data=request.POST)
+
+    form = authentication_form(request)
+
+    context = {
+        'form': form,
+    }
+    if extra_context is not None:
+        context.update(extra_context)
+    return render_to_response("registration/register.html", context)
