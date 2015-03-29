47d98c3aa334826be8ea9f38f7438d3a8d7a4662
Connor Young <cdy3xv@virginia.edu>
2015-03-29 15:08:36 -0400

    Registration Works

--- SecureWitness/Report/views.py
+++ SecureWitness/Report/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render_to_response
-
+from django.shortcuts import render
 from Report.models import reports
 
 
 def home(request):
     entries = reports.objects.all()[:20]
 
-    return render_to_response('index.html', {'report': entries})
+    return render(request, 'index.html', {'report': entries})
 
 
 def detail(request, pk):
-	return render_to_response('detail.html', {'report': reports.objects.all().filter(pk=pk)[0]})
+	return render(request, 'detail.html', {'report': reports.objects.all().filter(pk=pk)[0]})
