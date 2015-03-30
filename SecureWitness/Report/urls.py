d1f2e9b263c1ab7ec510e24849dababe3aba50fe
Vivian Liu <wl2nw@virginia.edu>
2015-03-30 16:15:48 -0400

    folders

--- SecureWitness/Report/urls.py
+++ SecureWitness/Report/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     url(r'^$', 'Report.views.home', name='home'),
     url(r'^detail/(\d+)/$', 'Report.views.detail', name='detail'),
     url(r'^add_report/$', 'Report.views.add_report', name='add_report'),
+#    url(r'^foler/$', 'Report.views.show_in_folder', name='folder_name')
     )
--- SecureWitness/Report/views.py
+++ SecureWitness/Report/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from Report.models import reports
+from Report.models import Folder
 
 
 def home(request):
     entries = reports.objects.all()[:20]
+    folders = Folder.objects.all()[:20]
 
     return render(request, 'index.html', {'report': entries})
+#    return render(request, 'index.html', {'folder': folders})
 
 
 def detail(request, pk):
@@ -13,3 +16,9 @@ def detail(request, pk):
 
 def add_report(request):
     return render(request, 'add_report.html')
+
+#def show_in_folder(request):
+#    reports_list = reports.objects.order_by('-timestamp')[:5]
+#    context = {'reports_list': reports_list}
+#    return render(request, 'report_list.html', context)
+
