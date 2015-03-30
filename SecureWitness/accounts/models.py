59b22d729bca5a4784145ff073c989cfa0a1ef96
Vivian Liu <wl2nw@virginia.edu>
2015-03-30 16:35:22 -0400

    groups

--- SecureWitness/accounts/models.py
+++ SecureWitness/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return self.user.username
+
+
+class UserGroup(models.Model):
+    group = models.OneToOneField(Group)
+
+    def __str__(self):
+        return self.group.name
+
