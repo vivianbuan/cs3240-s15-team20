f18726c3ae01b452a0222ac480fdc0c4003537c2
revanthkolli <rk8mt@virginia.edu>
2015-04-01 21:29:04 -0400

    Reports now add to database once submitted

--- SecureWitness/Report/models.py
+++ SecureWitness/Report/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import time
 
 
 class Folder(models.Model):
@@ -11,8 +12,14 @@ class Folder(models.Model):
 class reports(models.Model):
     author = models.CharField(max_length=30)
     title = models.CharField(max_length=100)
-    bodytext = models.TextField()
-    timestamp = models.DateTimeField()
+    short = models.TextField(max_length=100)
+    details = models.TextField()
+    location = models.CharField(max_length=100, null=True)
+    date = models.DateField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add = True)
+    keywords = models.CharField(max_length=100, null=True)
+    file = models.FileField(upload_to='documents/%Y/%m/%d', null=True)
+    private = models.BooleanField(default = False)
     folder = models.ForeignKey(Folder, default=1)
 
     def __str__(self) :
