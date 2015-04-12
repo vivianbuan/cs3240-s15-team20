3a6b907e32e3b027e6da007a448a74417ceb3e60
revanthkolli <revanthkolli@gmail.com>
2015-04-12 14:38:49 -0400

    Edit functionality works.

--- SecureWitness/SecureWitness/settings.py
+++ SecureWitness/SecureWitness/settings.py
@@ -77,10 +77,11 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
+DATE_FORMAT = "Y-m-d"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
