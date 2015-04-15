4af97b3636393bc756cc98a8efc53767a196c135
Yuri Bang <yb2fj@virginia.edu>
2015-04-14 21:30:52 -0400

    search function added

--- SecureWitness/Report/views.py
+++ SecureWitness/Report/views.py
@@ -10,7 +10,9 @@ from pprint import pprint
 import time
 from datetime import date
 from django.contrib.auth.decorators import login_required
-
+import re
+from django.db.models import Q
+from django.template import RequestContext
 
 
 def home(request):
@@ -116,3 +118,43 @@ def add_report(request):
 	entries = reports.objects.all()[:20]
 
 	return render(request,'add_report.html', {'report': entries})
+
+
+def normalize_query(query_string,
+                    findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+                    normspace=re.compile(r'\s{2,}').sub):
+
+    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
+
+def get_query(query_string, search_fields):
+    query = None
+    terms = normalize_query(query_string)
+    for term in terms:
+        or_query = None
+        for field_name in search_fields:
+            q = Q(**{"%s__icontains" % field_name: term})
+            if or_query is None:
+                or_query = q
+            else:
+                or_query = or_query | q
+        if query is None:
+            query = or_query
+        else:
+            query = query & or_query
+    return query
+
+def search(request):
+    query_string = ''
+    found_entries = None
+    if ('q' in request.GET) and request.GET['q'].strip():
+        query_string = request.GET['q']
+
+        entry_query = get_query(query_string, ['author', 'details', 'short',])
+
+        found_entries = reports.objects.filter(entry_query)
+
+    return render_to_response('search.html',
+                          { 'query_string': query_string, 'found_entries': found_entries },
+                          context_instance=RequestContext(request))
+
+
