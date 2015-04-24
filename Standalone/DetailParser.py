5aba2e8dd84dde6880599e7a93b190c96cf51804
Connor Young <cdy3xv@virginia.edu>
2015-04-24 16:03:19 -0400

    Standalone can actually login and can download files

---
+++ Standalone/DetailParser.py
@@ -0,0 +1,65 @@
+from html.parser import HTMLParser
+
+class DetailParser(HTMLParser):
+	reading="None"
+	encrypted=False
+	doc=""
+	docs=[]
+
+	def handle_starttag(self, tag, attrs):
+		# print("Start: " + tag)
+		for name,value in attrs:
+			if name == "id":
+				if value == "short":
+					self.reading="print"
+					print("Short Description: ", end="")
+				if value == "long":
+					self.reading="print"
+					print("Long Description: ", end="")
+				if value == "loc":
+					self.reading="print"
+					print("Location: ", end="")
+				if value == "date":
+					self.reading="print"
+					print("Date: ", end="")
+				if value == "keys":
+					self.reading="print"
+					print("Keywords: ", end="")
+				if "group" in value:
+					self.reading="print"
+					print("Shared with: ", end="")
+				if value == "private":
+					self.reading="print"
+					print("Is Private: ", end="")
+				if value == "enc" and tag == "ul":
+					self.encrypted=True
+					print("Documents are encrypted")
+				if "doc" in value:
+					self.reading="print"
+					string = "Document " + value.split('doc')[1] + ": "
+					print(string, end="")
+					self.docs.append(self.doc)
+			if name == "href" and tag == "a":
+				self.doc=value
+
+	def handle_endtag(self, tag):
+		if tag == "span":
+			if self.reading != "None":
+				print()
+			self.reading="None"
+
+	def handle_data(self, data):
+		if self.reading is "None":
+			pass
+		elif self.reading is "print":
+			print(data, end="")
+		else:
+			print("Error, Unhandled action")
+
+	def handle_charref(self, name):
+		if name.startswith('x'):
+			c = chr(int(name[1:], 16))
+		else:
+			c = chr(int(name))
+		if self.reading is 'print':
+			print(c, end="")
--- Standalone/main.py
+++ Standalone/main.py
@@ -1,25 +1,42 @@
 import urllib.request
 import urllib.parse
 from urllib.error import HTTPError
+from http import cookiejar
 from MainParser import MainParser, AuthParser
+from DetailParser import DetailParser
 import html
 import getpass
 
 _DEBUG = True
+cj = cookiejar.CookieJar()
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
 
-def getpage(url,username,password):
-	request = urllib.request.Request(url)
+def getpage(url,username,password, page):
+	urllib.request.install_opener(opener)
+
+	request = urllib.request.Request(url+'/accounts/login/')
+	request2 = urllib.request.Request(url+page)
 	# adding charset parameter to the Content-Type header.
 	request.add_header("Content-Type","application/x-www-form-urlencoded;charset=utf-8")
 	try:
-		with urllib.request.urlopen(request) as f:
+		with opener.open(request) as f:
+			"""for cookie in cj:
+				print(cookie)"""
 			csrf = f.getheader("Set-Cookie").split(";")
 			request.add_header("Cookie",f.getheader("Set-Cookie"))
 			item = csrf[0].split("=")[1]
-			data = urllib.parse.urlencode({'next': "/", 'username': username, 'password': password, 'csrfmiddlewaretoken': item})
+			data = urllib.parse.urlencode({'next': page, 
+										   'username': username,
+										   'password': password,
+										   'csrfmiddlewaretoken': item,
+										   'login':'login'})
 			data = data.encode('utf-8')
 			try:
-				with urllib.request.urlopen(request,data) as f2:
+				with opener.open(request,data) as f2:
+					"""for cookie in cj:
+						print(cookie)"""
+					pass
+				with opener.open(request2) as f2:
 					string = f2.read().decode('utf-8')
 					html.unescape(string)
 					#print(string)
@@ -32,25 +49,51 @@ def getpage(url,username,password):
 			print(e.read())
 
 def listreports(url, username, password):
-	string = getpage(url,username,password)
+	string = getpage(url,username,password,'/')
 	parser = MainParser()
 	parser.feed(string)
 	parser.close()
 
 
 def auth(url,username, password):
-	string = getpage(url,username,password)
+	string = getpage(url,username,password,'/')
 	parser = AuthParser()
+	# print(string)
 	parser.feed(string)
 	parser.close()
 	return parser.authenticated
+
+def detail(url, username, password, num):
+	string = getpage(url,username,password,'/reports/detail/'+num+'/')
+	parser = DetailParser()
+	# print(string)
+	parser.feed(string)
+	while(True):
+		fileno = int(input("~Download File(-1 to go back to main menu): "))
+		if fileno == -1:
+			break
+		if fileno >= len(parser.docs):
+			print("Incorrect input: That doc does not exist")
+			continue
+		filedownload(url, parser.docs[fileno],parser.encrypted)
+	parser.close()
+
+def filedownload(url,doc, enc):
+	address=url+doc
+	file_name = address.split('/')[-1]
+	with opener.open(address) as u:
+		with open(file_name, 'wb') as f:
+			if not enc:
+				f.write(u.read())
+			else:
+				print("time to decrypt")
 
 if __name__=="__main__":
 	while True:
 		url = input("Site URL: ")
 		username = input("Username: ")
 		pswd = getpass.getpass('Password:')
-		if auth(url + "/accounts/login/",username,pswd):
+		if auth(url,username,pswd):
 			loggedIn = True
 			break
 		while True:
@@ -68,14 +111,18 @@ if __name__=="__main__":
 			break
 
 	while loggedIn:
-		command = input("#Enter Command: ")
-		if command == "help":
-			print("There are currently 2 commands implemented:")
+		command = input("~Enter Command: ")
+		command = command.split(' ')
+		if command[0] == "help":
+			print("There are currently 3 commands implemented:")
 			print("\tlist: list all reports accessible to you")
+			print("\tdetail [x]: list details for report x from list")
 			print("\tquit: exit this program")
-		elif command == "list":
-			listreports(url+"/accounts/login/",username,pswd)
-		elif command == "quit":
+		elif command[0] == "list":
+			listreports(url,username,pswd)
+		elif command[0] == "detail":
+			detail(url,username,pswd,command[1])
+		elif command[0] == "quit":
 			break
 		else:
 			print("Command not recognized. Type help for list of commands")
