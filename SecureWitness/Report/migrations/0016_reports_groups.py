bc3853e047693b5799ad9eb7df931f61ab0bc543
revanthkolli <revanthkolli@gmail.com>
2015-04-24 14:19:17 -0400

    Fixing merges

---
+++ SecureWitness/Report/migrations/0016_reports_groups.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auth', '0006_require_contenttypes_0002'),
+        ('Report', '0015_reports_encrypt'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='reports',
+            name='groups',
+            field=models.ManyToManyField(to='auth.Group'),
+        ),
+    ]
